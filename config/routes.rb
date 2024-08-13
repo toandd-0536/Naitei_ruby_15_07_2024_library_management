93d3a3f75864b99ec9ecd5cbbaf2955a2ab8e979
duongngt-0538 <nguyen.gia.tung.duong@sun-asterisk.com>
2024-08-13 13:44:34 +0700

    [Admin] Admin namespace + layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-   root "users#show"
-   get "/login", to: "sessions#new"
-   post "/login", to: "sessions#create"
+  root "users#show"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  scope "(:locale)", locale: /en|vi/ do
+    namespace :admin do
+      resources :users
+    end
+  end
 end
