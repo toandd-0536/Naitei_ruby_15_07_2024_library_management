806af097a02ad0bcd4d69458f2d5bd8aa0247a96
duongngt-0538 <nguyen.gia.tung.duong@sun-asterisk.com>
2024-08-21 17:25:17 +0700

    [User] Favorite author

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,11 @@ module ApplicationHelper
     ActionView::Base.field_error_proc = original_field_error_proc
   end
 
-  def nav_link_to path, class_active = "active", options = {}, &block
-    active_class = request.path.start_with?(path) ? class_active : ""
+  def nav_link_to paths, class_active = "active", options = {}, &block
+    paths = [paths] unless paths.is_a?(Array)
+    active_class = paths.any?{|path| current_page?(path)} ? class_active : ""
     options[:class] = [options[:class], active_class].compact.join(" ")
 
-    link_to path, options, &block
+    link_to paths.first, options, &block
   end
 end
